70ba2dcc774ede07ce5ba9b0cb5aae7c67b5cd2d
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-11-10 17:33:57 +0300

    Initial commit

---
+++ washdisher/src/main/java/goodr0ne/washdisher/WashdisherApplication.java
@@ -0,0 +1,12 @@
+package goodr0ne.washdisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashdisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashdisherApplication.class, args);
+	}
+}
---
+++ washdisher/src/test/java/goodr0ne/washdisher/WashdisherApplicationTests.java
@@ -0,0 +1,16 @@
+package goodr0ne.washdisher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WashdisherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
